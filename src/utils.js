e775437fe3c1f74bc66a07ce9589ecd71803a3c2
Styx <styx5066@gmail.com>
2020-03-14 10:23:21 -0500

    Add icon

--- src/utils.js
+++ src/utils.js
@@ -13,7 +13,7 @@ function devMode() {
 // DESCRIPTION: Determines whether dev options appear
 //---------------
 function version() {
-  return "0.2.0";
+  return "0.2.10";
 }
 
 // ------------------------------------------------------
